dc22b7a00ab5499d93612d24a50a4a52a598ebca
Sebs5384 <operationmadjackal@hotmail.com>
2024-06-09 18:47:27 -0300

    Clean up main files
    We'll be deleting creat-react-app default files and creating on our structure for our project

---
+++ src/pages/Pokedex.js
@@ -0,0 +1,17 @@
+function Pokedex() {
+    return (
+        <>
+            <div>
+                <h1>Banner</h1>
+            </div>
+            <div>
+                <h1>Board</h1>
+            </div>
+            <div>
+                <h1>Paginator</h1>
+            </div>
+        </>
+    );
+};
+
+export default Pokedex;
