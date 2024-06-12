a4718a339d887a459366680cade76d844531ccbd
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-06-11 20:13:45 -0600

    Update grid styled components
    We'll be using these to build our board for our game

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -7,20 +7,45 @@ const Section = styled.section`
     margin-top: 0.5rem;
 `;
 
-const Board = styled.div`
+const Wrapper = styled.div`
+    width: 100%;
+    max-width: 1480px;
+    margin: 0 auto;
+    padding: 1rem;
     background-color: #fff8dc;
     border: 3px solid rgb(250, 239, 176);
     border-style: outset;
     border-radius: 8px;
 `;
 
-function Grid({}){
-    
+const Body = styled.div`
+    padding: 1rem;
+`;
+
+const Board = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+`;
+
+function Grid({ cards }){
     return(
         <Section>
-            <Board className="col-12 col-md-11 card-border text-center">
-                
-            </Board>
+            <Wrapper>
+                <Body>
+                    <Board>
+                        <Card cardKey={"bulbasaur"} className={"card col-2"}/>
+                        <Card cardKey={"bulbasaur"} className={"card col-2"}/>
+                        <Card cardKey={"bulbasaur"} className={"card col-2"}/>
+                        <Card cardKey={"bulbasaur"} className={"card col-2"}/>
+                        <Card cardKey={"bulbasaur"} className={"card col-2"}/>
+                        <Card cardKey={"bulbasaur"} className={"card col-2"}/>
+                        <Card cardKey={"bulbasaur"} className={"card col-2"}/>
+                        <Card cardKey={"bulbasaur"} className={"card col-2"}/>
+                    </Board>
+                </Body>
+            </Wrapper>
         </Section>
     );
 };
