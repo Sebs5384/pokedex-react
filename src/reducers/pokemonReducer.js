a74ec5e64225eb46ea036b8f572bbcd43fa2f246
Sebs5384 <operationmadjackal@hotmail.com>
2024-06-13 20:57:53 -0300

    Creates reducer for the api call
    We'll make use of this file to dispatch our states on our hooks, since all the api calls we do to the pokeapi are just returning data in a json format we are not gonna need another cases others than these 3

---
+++ src/reducers/pokemonReducer.js
@@ -0,0 +1,40 @@
+const intialState = {
+    loading: false,
+    data: null,
+    error: null
+};
+
+const pokemonReducer = (state = intialState, action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case "FETCH_REQUEST":
+            return {
+                ...state,
+                loading: true,
+                data: null,
+                error: null
+            };
+        case "FETCH_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                data: payload,
+                error: null
+            };
+        case "FETCH_FAILURE":
+            return {
+                ...state,
+                loading: false,
+                data: null,
+                error: payload
+            };
+        default:
+            return state;
+    };
+};
+
+export {
+    pokemonReducer,
+    intialState
+};
