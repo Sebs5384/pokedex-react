9fb779162649d676cd78ee1095d2e92c677a169d
Sebs5384 <operationmadjackal@hotmail.com>
2024-06-15 21:13:40 -0300

    fetches pokemons to test out

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,12 +1,19 @@
+import useFetchPokemons from "../hooks/useFetchPokemons";
 import Grid from "../components/Grid";
+import { useEffect } from "react";
 
 function Pokedex() {
+    const { loading, pokemons, error } = useFetchPokemons();
+    useEffect(() => {
+        console.log(pokemons);
+    })
+
     return (
         <>
             <div>
                 <h1>Banner</h1>
             </div>
-            <Grid />
+            <Grid cards={pokemons}/>
             <div>
                 <h1>Paginator</h1>
             </div>
