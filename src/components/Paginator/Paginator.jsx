c5cbe901e4506aeef12b057a3281dae9326d90f0
Sebs5384 <operationmadjackal@hotmail.com>
2024-06-17 17:53:24 -0300

    Creates PaginationButton component and replaces dummy structure
    We'll be using this component to specify the buttons on our pagination, normally it will be 3 pages on display and Previous and Next button

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -1,31 +1,30 @@
 import styled from "@emotion/styled";
+import PaginatorButton from "./PaginatorButton";
 
-const Footer = styled.footer`
+const PaginatorContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 0.5rem;
 `;
 
+const Pagination = styled.nav`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
 function Paginator() {
     return (
-        <Footer>
-            <div className="row">
-                <nav>
-                    <ul className="pagination justify-content-center">
-                        <li className="page-item">
-                            <a className="page-link">Previous</a>
-                        </li>
-                        <li className="page-item">
-                            <a className="page-link">1</a>
-                        </li>
-                        <li className="page-item">
-                            <a className="page-link">Next</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </Footer>
+        <PaginatorContainer className="pagination justify-content-center">
+            <Pagination className="page-item">
+                <PaginatorButton>Previous</PaginatorButton>
+                <PaginatorButton>1</PaginatorButton>
+                <PaginatorButton>2</PaginatorButton>
+                <PaginatorButton>3</PaginatorButton>
+                <PaginatorButton>Next</PaginatorButton>
+            </Pagination>
+        </PaginatorContainer>
     );
 };
 
---
+++ src/components/Paginator/PaginatorButton.jsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+const PaginationLink = styled.a`
+    margin: 0.1rem;
+    color: black;
+    &:hover {
+        color: black;
+        border-color: #dc143c;
+        box-shadow: 0 0 10px #dc143c;
+    }
+`;
+
+function PaginationButton({ children, onClick = () => {} }) {
+    return (
+        <PaginationLink className="page-link" href="#" onClick={onClick}>{children}</PaginationLink>
+    );
+};
+
+export default PaginationButton;
