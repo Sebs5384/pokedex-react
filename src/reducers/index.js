d4bbf2f12bfc9e35b180c48aa18381b070f325dc
Sebs5384 <operationmadjackal@hotmail.com>
2024-06-21 23:38:26 -0300

    Creates an index for the reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { pokemonReducer, initialPokemonState } from "./pokemonReducer";
+import { paginationReducer, initialPaginationState } from "./paginationReducer";
+
+export {
+    pokemonReducer,
+    initialPokemonState,
+    paginationReducer,
+    initialPaginationState
+};
