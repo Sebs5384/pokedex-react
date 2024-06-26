2302da3d4c1a6617ee85759feaf6c0a0a47160ca
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-06-25 22:38:24 -0600

    Set total pages inside our effect

--- src/hooks/useTotalPages.js
+++ src/hooks/useTotalPages.js
@@ -4,21 +4,21 @@ import { totalPagesReducer, initialTotalPagesState } from "../reducers/index";
 function useTotalPages(ITEMS_PER_PAGE, items) {
     const [state, dispatch] = useReducer(totalPagesReducer, initialTotalPagesState);
 
-    const setTotalPages = (totalCount) => {
-        const totalPages = Array.from({ length: Math.ceil(totalCount / ITEMS_PER_PAGE) }, (_, index) => index + 1);
-        dispatch({ type: "SET_TOTAL_PAGES", payload: totalPages });
-    };
-
     useEffect(() => {
+        const setTotalPages = (totalCount) => {
+            const totalPages = Array.from({ length: Math.ceil(totalCount / ITEMS_PER_PAGE) }, (_, index) => index + 1);
+            dispatch({ type: "SET_TOTAL_PAGES", payload: totalPages });
+        };
+
         if (items) {
             const totalItems = items.count;
             setTotalPages(totalItems);
-        }
-    }, [items]);
+        };
+    }, [items, ITEMS_PER_PAGE]);
 
     return {
         totalPages: state.totalPages,
-    }
+    };
 };
 
 export default useTotalPages;
