df039d06a8e6d54494f4dd430d9da6edaebd6633
Sebs5384 <operationmadjackal@hotmail.com>
2024-06-28 19:58:30 -0300

    Update api file with a new fetch function
    This function will be in charge of fetching the data for each pokemon in particular through its ID

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,16 +1,34 @@
 const URL = "https://pokeapi.co/api/v2";
 
 async function getPokemons(limit, offset) {
-    const pokemonsURL = `${URL}/pokemon?limit=${limit}&offset=${offset}`;
+    if(limit || offset === undefined) throw new Error(`limit and offset are required, reading: ${limit}, ${offset}`);
 
+    const pokemonsURL = `${URL}/pokemon?limit=${limit}&offset=${offset}`;
     return await fetch(pokemonsURL)
         .then(response => response.json())
         .catch((error) => {
             throw new Error(error);
         })
         .finally(() => {
-            console.log(`Warning, using API call URL: ${pokemonsURL}`);
+            console.error(`Warning, using API call URL: ${pokemonsURL}`);
         });
 };
 
-export default getPokemons;
+async function getPokemon(id) {
+    if(id === undefined) throw new Error(`id is required, reading: ${id}`);
+
+    const pokemonURL = `${URL}/pokemon/${id}`;
+    return await fetch(pokemonURL)
+        .then(response => response.json())
+        .catch((error) => {
+            throw new Error(error);
+        })
+        .finally(() => {
+            console.error(`Warning, using API call URL: ${pokemonURL}`);
+        });
+};
+
+export {
+    getPokemons,
+    getPokemon
+};
