1523922c855548892014f2b4731ca54cb4a07a57
Sebs5384 <operationmadjackal@hotmail.com>
2024-07-16 23:37:53 -0300

    Update utils file with two new algorithms
    We'll use to convert the weight and height of the pokemon, the api response only grant us of cm and decimeter

--- src/utils/general.js
+++ src/utils/general.js
@@ -12,6 +12,14 @@ function getPokemonSpriteUrl(url) {
     return pokemonSpriteUrl;
 };
 
+function convertGramToLb(grams) {
+    return (grams * 0.00220462).toFixed(2);
+};
+  
+function convertDecimeterToFeet(meters) {
+    return (meters * 0.328084).toFixed(2).replace('.', "'");
+};
+
 export {
     setItemRange,
     getPokemonSpriteUrl,
