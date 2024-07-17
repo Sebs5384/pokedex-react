ee6f3fec67833fe1a5f6eeab3b1e1cd0ffa4c7a9
Sebs5384 <operationmadjackal@hotmail.com>
2024-07-16 23:38:20 -0300

    Add parsers to util pokemon file
    This will clean up the data that we'll use inside the modal card

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -9,10 +9,10 @@ function parsePokemonData(data) {
     const height = convertDecimeterToFeet(pokemon.height);
     const weight = convertGramToLb(pokemon.weight);
     const typeAdvantage = getPokemonAdvantage(types.mainType, advantageChart);
-    const previousEvolutions = getPreviousEvolutions(species);
+    const previousEvolutions = getPreviousEvolutionString(species);
     const description = getDescription(species.flavor_text_entries, "en");
 
-    return {
+    const pokemonData = {
         id,
         name,
         skills,
@@ -24,4 +24,65 @@ function parsePokemonData(data) {
         previousEvolutions,
         description
     };
+
+    return pokemonData;
 };
+
+function parsePokemonName(name) {
+    const words = name.split('-');
+    const pokemonName = words[0].lenght === 2 ? `${words[0]}${words[1]}` : words[0];
+
+    return  pokemonName;
+};
+
+function getPokemonSkills(skills) {
+    const pokemonSkills = {
+        firstSkill: skills[0].ability.name,
+        secondSkill: skills[1].ability.name ? skills[1].ability.name : ''
+    };
+    
+    return pokemonSkills; 
+};
+
+function getPokemonStats(stats) {
+    const pokemonStats = {
+        hp: stats[0].base_stat,
+        attack: stats[1].base_stat,
+        defense: stats[2].base_stat,
+        spAtk: stats[3].base_stat,
+        spDef: stats[4].base_stat,
+        speed: stats[5].base_stat
+    };
+
+    return pokemonStats; 
+};
+
+function getPokemonTypes(types) {
+    const pokemonTypes = {
+        mainType: types[0].type.name,
+        secondaryType: types[1].type.name ? types[1].type.name : undefined
+    };
+    
+    return pokemonTypes;
+};
+
+function getPreviousEvolutionString(species) {
+    const previousEvolutions = {
+        name: species.evolves_from_species ? `Evolves from ${species.evolves_from_species.name}` : 'Basic Pokemon',
+        id: species.evolves_from_species ? species.evolves_from_species.url.split('/')[6] : 'None',
+        genus: species.genera.length ? species.genera.find((genus) => genus.language.name === 'en').genus : '',
+    };
+
+    return previousEvolutions;
+};
+
+function getDescription(entry, language) {
+    const pokemonTextEntry = entry;
+    const entry = pokemonTextEntry.find((pokemonEntry) => pokemonEntry.language.name === "en");
+    
+    const pokemonEntry = entry ? entry.flavor_text.replace(/\u000c/g, ' ') : '';
+    
+    return pokemonEntry;
+};
+
+export default parsePokemonData;
