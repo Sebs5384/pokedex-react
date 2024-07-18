261aa81ed44b86edb7eb511cd4832c3c7ad04730
Sebs5384 <operationmadjackal@hotmail.com>
2024-07-17 21:15:36 -0300

    Replaces old object for payload

--- src/reducers/selectedCardReducer.js
+++ src/reducers/selectedCardReducer.js
@@ -15,25 +15,7 @@ function selectedCardReducer(state = initialSelectedCardState, action) {
         case "SET_CARD_DATA": 
             return {
                 ...state,
-                data: {
-                    ...state.data,
-                    previousEvolution: payload.species.evolves_from_species.name ? payload.species.evolves_from_species.name : "",
-                    genus: payload.species.genera[7].genus,
-                    name: payload.pokemon.name,
-                    hp: payload.pokemon.stats[0].base_stat,
-                    height: payload.pokemon.height,
-                    weight: payload.pokemon.weight,
-                    firstSkill: payload.pokemon.abilities[0].ability.name,
-                    secondSkill: payload.pokemon.abilities[1].ability.name,
-                    attack: payload.pokemon.stats[1].base_stat,
-                    defense: payload.pokemon.stats[2].base_stat,
-                    speed: payload.pokemon.stats[5].base_stat,
-                    spAttack: payload.pokemon.stats[3].base_stat,
-                    spDefense: payload.pokemon.stats[4].base_stat,
-                    description: payload.species.flavor_text_entries[0].flavor_text,
-                    id: payload.pokemon.id,
-                    type: payload.pokemon.types[0].type.name
-                }
+                data: payload
             };
         case "RESET_CARD_DATA": 
             return {
