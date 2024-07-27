69921e7701deab6cb0c5652006e071e1d3b3492b
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-07-27 05:25:27 -0600

    Add texture files and index
    We'll use this textures as the background of each card depending of its type

---
+++ src/assets/img/modal-texture/index.js
@@ -0,0 +1,39 @@
+import bugTexture from "./bug-texture.png";
+import darkTexture from "./dark-texture.png";
+import dargonTexture from "./dragon-texture.png";
+import electricTexture from "./electric-texture.png";
+import fairyTexture from "./fairy-texture.png";
+import fighthingTexture from "./fighting-texture.png";
+import fireTexture from "./fire-texture.png";
+import flyingTexture from "./flying-texture.png";
+import ghostTexture from "./ghost-texture.png";
+import grassTexture from "./grass-texture.png";
+import groundTexture from "./ground-texture.png";
+import iceTexture from "./ice-texture.png";
+import normalTexture from "./normal-texture.png";
+import poisonTexture from "./poison-texture.png";
+import psychicTexture from "./psychic-texture.png";
+import rockTexture from "./rock-texture.png";
+import steelTexture from "./steel-texture.png";
+import waterTexture from "./water-texture.png";
+
+export { 
+    bugTexture, 
+    darkTexture, 
+    dargonTexture, 
+    electricTexture, 
+    fairyTexture, 
+    fighthingTexture, 
+    fireTexture, 
+    flyingTexture, 
+    ghostTexture, 
+    grassTexture, 
+    groundTexture, 
+    iceTexture, 
+    normalTexture, 
+    poisonTexture, 
+    psychicTexture, 
+    rockTexture, 
+    steelTexture, 
+    waterTexture
+};
