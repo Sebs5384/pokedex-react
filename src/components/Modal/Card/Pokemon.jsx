0dd0a1762862dda40b81b4e2e6e24236cd4756e7
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-07-28 13:49:45 -0600

    Creates a file for the styled components in pokemonCard

---
+++ src/components/Modal/Card/Pokemon.jsx
@@ -0,0 +1,108 @@
+import { Modal } from "react-bootstrap";
+import styled from "@emotion/styled";
+
+const PokemonModal = styled(Modal)`
+    .modal-content {
+        border: solid gold 12px;
+        border-radius: 10px;
+        border-style: outset;
+        background-image: url(${props => props.backgroundImage});
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+        
+    .modal-title {
+        margin-top: -8px;
+    }
+
+    .hp-text {
+        color: red;
+    }
+
+    .pokemon-species-image {
+        width: 11%;
+        height: 11%;
+        margin: 0 auto;
+    }
+`;
+
+const CardSection = styled.section`
+    &.card-banner {
+        border: 2px solid rgb(254, 230, 98);
+        background: content-box radial-gradient(rgb(250, 232, 130), rgb(193, 187, 26));
+        border-style: inset;
+    }
+`;
+
+const PokemonContainer = styled.div`
+    .card-border {
+        border: 8px solid rgb(254, 230, 98);
+        margin-top: -5px;
+        border-style: outset;
+        border-radius: 0px;
+    }
+
+    .description-border {
+        border: 2px solid rgb(254, 230, 98);
+        border-style: outset;
+        border-radius: 0px;
+        background-color: transparent;
+    }
+
+    .stats-icon {
+        width: 20px;
+        height: 20px;
+        margin-top: -5px;
+        margin-right: 5px;
+    }
+`;
+
+const PokemonCardText = styled.strong`
+    text-transform: capitalize;
+
+    &.species-font {
+        font-size: 11px;
+    }
+
+    &.title-font {
+        font-size: 33px;
+    }
+
+    &.banner-font {
+        font-size: 11px;
+    }
+
+    &.status-font {
+        font-size: 11px;
+    }
+
+    &.skill-font {
+        color: red;
+        font-size: 15px;
+        text-shadow: -0.3px -0.3px 0 black, 0.3px -0.3px 0 black, -0.3px 0.3px 0 black, 0.3px 0.3px 0 black;
+    }
+
+    &.footer-font {
+        font-size: 10px;
+    }
+`;
+
+const CardTopSeparator = styled.hr`
+    border: 1px solid var(--bs-warning);
+    opacity: 0.5;
+    margin: auto;
+`;
+
+const CardSeparator = styled.hr`
+    border: 1px solid black;
+`;
+
+export {
+    PokemonModal,
+    CardSection,
+    PokemonContainer,
+    PokemonCardText,
+    CardTopSeparator,
+    CardSeparator
+}
