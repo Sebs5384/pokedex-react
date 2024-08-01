b3e0928a95343d7dcf84becc48a508c4304f6571
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-08-01 16:34:02 -0600

    Update PokemonCard with a condition to show up the loading modal

--- src/components/Modal/Card/PokemonCard.jsx
+++ src/components/Modal/Card/PokemonCard.jsx
@@ -2,16 +2,21 @@ import { PokemonModal } from "./Pokemon";
 import PokemonCardHeader from "./PokemonCardHeader";
 import PokemonCardBody from "./PokemonCardBody";
 import PokemonCardFooter from "./PokemonCardFooter";
+import LoadingPokemonCard from "./LoadingPokemonCard";
 
-function PokemonCard({show, handleClose, selectedCard, pokemonSprite}) { 
+function PokemonCard({visibility, handleClose, selectedCard, pokemonSprite, loading}) { 
     if(!selectedCard) return null;
 
     return(
-        <PokemonModal show={show} onHide={handleClose} backgroundTexture={selectedCard.backgroundTexture}>   
-            <PokemonCardHeader selectedCard={selectedCard} pokemonSprite={pokemonSprite} closeModal={handleClose}/>
-            <PokemonCardBody selectedCard={selectedCard} pokemonSprite={pokemonSprite}/>
-            <PokemonCardFooter selectedCard={selectedCard} pokemonSprite={pokemonSprite}/>
-        </PokemonModal>
+        <>
+            {loading ? <LoadingPokemonCard showModal={loading} /> : 
+                <PokemonModal show={visibility} onHide={handleClose} backgroundTexture={selectedCard.backgroundTexture}>   
+                    <PokemonCardHeader selectedCard={selectedCard} pokemonSprite={pokemonSprite} closeModal={handleClose}/>
+                    <PokemonCardBody selectedCard={selectedCard} pokemonSprite={pokemonSprite}/>
+                    <PokemonCardFooter selectedCard={selectedCard} pokemonSprite={pokemonSprite}/>
+                </PokemonModal>
+            }
+        </>
     );
 };
 
