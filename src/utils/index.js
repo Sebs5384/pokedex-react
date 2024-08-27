9f8c454c4860837340ec50637a9e0871e2150954
Sebs5384 <operationmadjackal@hotmail.com>
2024-08-27 11:39:51 -0300

    Creates an index file for utils
    We'll be exporting all the exported modules through this one

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+import { parsePokemonData, getPokemonSpriteUrl, getPokemonNames } from "./pokemon";
+import { setItemRange, convertDecimeterToFeet, convertGramToLb, randomizeNumber } from "./general";
+
+export {
+    parsePokemonData,
+    getPokemonSpriteUrl,
+    getPokemonNames,
+    setItemRange,
+    convertDecimeterToFeet,
+    convertGramToLb,
+    randomizeNumber
+};
