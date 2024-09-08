518b3795b15d3f623d8298d768ab95c2264c7bbc
Sebs5384 <operationmadjackal@hotmail.com>
2024-09-08 15:42:12 -0300

    Update CatchPokemon component by cleaning it and adding props

--- src/components/Navbar/CatchPokemon.jsx
+++ src/components/Navbar/CatchPokemon.jsx
@@ -3,39 +3,29 @@ import ImageContainer from "../shared/ImageContainer";
 import pokeBackground from "../../assets/img/misc/poke-background.png";
 import pokemonLen from "../../assets/img/misc/pokedex-len.png";
 import catchPokemon from "../../assets/img/misc/catch-a-pokemon.png";
-import { useState, useEffect } from "react";
-import { useFetchPokemon } from "../../hooks/index";
 
-function CatchPokemon({ selectPokemon }) {
-    const [isShaking, setIsShaking] = useState(false);
-    const [randomNumber, setRandomNumber] = useState(null);
-    const { randomPokemon } = useFetchPokemon(randomNumber);
-    const handleClick = () => {
-        setIsShaking(true);
-        setRandomNumber(Math.floor(Math.random() * 100) + 1);
-        setTimeout(() => {
-            setIsShaking(false);
-        }, 6000);
-    };
-
-    useEffect(() => {
-        console.log(randomPokemon);
-    }, [randomPokemon]);
-
+function CatchPokemon({ selectPokemon, isShaking, caughtPokemons, handlePokeballClick }) {
     return(
         <>
             <PokeballButton 
                 className={isShaking ? "poke-shake" : ""}
                 backgroundImage={pokeBackground}
-                onClick={handleClick}
+                onClick={handlePokeballClick}
             />
             <NavContainer className="catch-pokemon">
                 <ImageContainer src={catchPokemon}></ImageContainer>
             </NavContainer>
             <NavContainer className="poke-slot">
-                <ImageContainer src={randomNumber ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomNumber}.png` : pokemonLen } ></ImageContainer>
-                <ImageContainer src={randomNumber ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomNumber}.png` : pokemonLen } ></ImageContainer>
-                <ImageContainer src={randomNumber ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomNumber}.png` : pokemonLen } ></ImageContainer>
+                    {caughtPokemons.map((pokemon, index) => (
+                        pokemon ? 
+                        <ImageContainer 
+                            key={index}
+                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                            onClick={() => selectPokemon(pokemon.name)} 
+                        /> :
+                        <ImageContainer src={pokemonLen} 
+                        />
+                    ))}
             </NavContainer>
         </>
     );
