e15547a90901ff56cf5c7678a90648ada32203b4
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-09-10 20:08:49 -0600

    Replace cardText component for registrationText component

--- src/components/Modal/CatchPokemon/CaughtPokemonAlert.jsx
+++ src/components/Modal/CatchPokemon/CaughtPokemonAlert.jsx
@@ -1,23 +1,23 @@
-import { PokemonCardText, PokemonContainer, PokemonAlertModal } from "../Pokemon";
+import { PokemonRegistrationText, PokemonContainer, PokemonAlertModal } from "../Pokemon";
 import cx from "classnames";
 
 function CaughtPokemonAlert({ showModal, textChange, topText, bottomText }) {
     return (
-        <PokemonAlertModal show={showModal} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+        <PokemonAlertModal show={showModal} className="emerald-font" size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
             <PokemonContainer className="modal-body">
                 <PokemonContainer className="alert-background d-flex justify-content-center align-items-center">
                     <PokemonContainer className="row w-100 justify-content-center">
                         <PokemonContainer className="col-12 text-start align-items-start alert-text-background fixed-height d-flex flex-column">
-                            <PokemonCardText className={cx("emerald-font", {
+                            <PokemonRegistrationText className={cx({
                                 "typewriter-effect": textChange
                             })}>
                                 {topText}
-                            </PokemonCardText>
-                            <PokemonCardText className={cx("emerald-font", {
+                            </PokemonRegistrationText>
+                            <PokemonRegistrationText className={cx({
                                 "typewriter-effect-delayed": textChange
                             })}>
                                 {bottomText}
-                            </PokemonCardText>
+                            </PokemonRegistrationText>
                         </PokemonContainer>
                     </PokemonContainer>
                 </PokemonContainer>
