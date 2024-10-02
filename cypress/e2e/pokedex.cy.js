0a534f968ac223f8e8702df622f6aaf80de2f0ed
Sebs5384 <operationmadjackal@hotmail.com>
2024-10-02 20:34:22 -0300

    Add initial landing test to the pokedex
    This is being done to test out that main components are not failing to load as well as we are intercepting and mocking the API requests made at the very begin

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,40 @@
+describe("Pokedex loading test", () => {
+  const localHost = "http://localhost:3000";
+  const firstPage = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+  const pokemonList = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+
+  beforeEach(() => {
+    cy.visit(localHost);
+
+    cy.intercept("GET", firstPage, {
+      fixture: "pokedexFirstPage.json",
+    }).as("firstPage");
+
+    cy.intercept("GET", pokemonList, {
+      fixture: "pokemonList.json",
+    }).as("pokemonList");
+  });
+
+  it("Should load the initial pokedex components and data correctly", () => {
+    cy.wait("@firstPage").then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      expect(interception.response.body.count).to.eq(1302);
+      expect(interception.response.body.results.length).to.eq(20);
+    });
+
+    cy.wait("@pokemonList").then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      expect(interception.response.body.count).to.eq(1302);
+    });
+    
+    cy.get("[data-cy='navbar-section']").should("be.visible");
+    cy.get("[data-cy='grid-section']").should("be.visible");
+    cy.get("[data-cy='pagination-section']").should("be.visible");
+  
+    cy.get("[data-cy='loading-pokemon-alert']").should("not.exist");
+    cy.get("[data-cy='loading-grid-spinner']").should("not.exist");
+    cy.get("[data-cy='caught-pokemon-alert-modal']").should("not.exist");
+    cy.get("[data-cy='caught-pokemon-summary-modal']").should("not.exist");
+    cy.get("[data-cy='pokemon-card-modal']").should("not.exist");
+  });
+});
