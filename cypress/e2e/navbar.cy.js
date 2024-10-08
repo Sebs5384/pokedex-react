207f790fd97559ee7b4c4187007355c30996bbb0
Sebs5384 <operationmadjackal@hotmail.com>
2024-10-08 20:15:23 -0300

    Refactors navbar cy test case

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -1,13 +1,14 @@
 describe("Navbar interaction testing", () => {
     const localHost = "http://localhost:3000";
     const pokemonList = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
-    const blastoiseUrl = "https://pokeapi.co/api/v2/pokemon/blastoise";
-    const blastoiseSpeciesUrl = "https://pokeapi.co/api/v2/pokemon-species/blastoise";
+    const pokemonUrl = (category, pokemon) => {
+        return `https://pokeapi.co/api/v2/${category}/${pokemon}`;
+    };
 
     beforeEach(() => {
         cy.visit(localHost);
-
         window.localStorage.clear();
+
         cy.intercept("GET", pokemonList, (req) => {
             req.reply({
                 fixture: "pokemonList.json"
@@ -16,17 +17,18 @@ describe("Navbar interaction testing", () => {
     });
 
     it("Should reload the website when clicking on the logo", () => {
-        cy.get("[data-cy='navbar-logo']").should("exist");
-        cy.get("[data-cy='navbar-section").should("be.visible");
-        cy.get("[data-cy='grid-section']").should("be.visible");
-        cy.get("[data-cy='pagination-section']").should("be.visible");
+        cy.get("[data-cy='navbar-logo']").as("navbarLogo").should("exist");
+        cy.get("[data-cy='navbar-section").as("navbarSection").should("be.visible");
+        cy.get("[data-cy='grid-section']").as("gridSection").should("be.visible");
+        cy.get("[data-cy='pagination-section']").as("paginationSection").should("be.visible");
 
         cy.wait(2000);
-        cy.get("[data-cy='navbar-logo']").click();
+        cy.get("@navbarLogo").click();
+
         cy.location("pathname").should("eq", "/").then(() => {
-            cy.get("[data-cy='navbar-section']").should("be.visible");
-            cy.get("[data-cy='grid-section']").should("be.visible");
-            cy.get("[data-cy='pagination-section']").should("be.visible");
+            cy.get("@navbarSection").should("be.visible");
+            cy.get("@gridSection").should("be.visible");
+            cy.get("@paginationSection").should("be.visible");
         });
     });
 
@@ -38,23 +40,23 @@ describe("Navbar interaction testing", () => {
         cy.wait("@pokemonList").then((interception) => {
             expect(interception.response.statusCode).to.eq(200);
 
-            cy.get("[data-cy='navbar-dropdown-menu']").should("exist");
-            cy.get("[data-cy='navbar-dropdown-menu']").should("be.visible");
-            cy.get("[data-cy='navbar-dropdown-menu']").find("a").should("have.length", interception.response.body.results.length);
-            cy.get("[data-cy='navbar-section']").click();
-
-            cy.get("[data-cy='navbar-dropdown-menu']").should("not.exist");
+            cy.get("[data-cy='navbar-dropdown-menu']").as("navbarDropdownMenu").should("exist").then(() => {
+                cy.get("@navbarDropdownMenu").should("be.visible");
+                cy.get("@navbarDropdownMenu").find("a").should("have.length", interception.response.body.results.length);
+                cy.get("@navbarDropdownMenu").click();
+                cy.get("@navbarDropdownMenu").should("not.exist");
+            });
         });
     });
 
     it("Should search for a pokemon through typing in the searchbox and then displaying the card when selecting it", () => {
-        cy.intercept("GET", blastoiseUrl, (req) => {
+        cy.intercept("GET", pokemonUrl("pokemon", "blastoise"), (req) => {
             req.reply({
                 fixture: "blastoise.json"
             });
         }).as("blastoise");
 
-        cy.intercept("GET", blastoiseSpeciesUrl, (req) => {
+        cy.intercept("GET", pokemonUrl("pokemon-species", "blastoise"), (req) => {
             req.reply({
                 fixture: "blastoiseSpecies.json"
             });
@@ -65,13 +67,13 @@ describe("Navbar interaction testing", () => {
         cy.get("[data-cy='loading-pokemon-alert']").should("not.exist");
         cy.get("[data-cy='pokemon-card-modal']").should("not.exist");
         
-        cy.get("[data-cy='navbar-search-input']").click();
-        cy.get("[data-cy='navbar-search-input']").type("bLasToIsE");
-        cy.get("[data-cy='blastoise']").click();
+        cy.get("[data-cy='navbar-search-input']").as("searchboxSearchInput").click().then(() => {
+            cy.get("@searchboxSearchInput").type("bLasToIsE");
+            cy.get("[data-cy='blastoise']").click();
+        });
 
-        cy.get("[data-cy='loading-pokemon-alert']").should("exist").then(() => {
-            cy.get("[data-cy='loading-pokemon-alert']").should("be.visible");
-            cy.get("[data-cy='loading-pokemon-alert']").should("not.exist");
+        cy.get("[data-cy='loading-pokemon-alert']").as("loadingAlert").should("exist").then(() => {
+            cy.get("@loadingAlert").should("be.visible");
         });
 
         cy.wait("@blastoise").then((interception) => {
@@ -82,38 +84,41 @@ describe("Navbar interaction testing", () => {
             expect(interception.response.statusCode).to.eq(200);
         });
 
-        cy.get("[data-cy='pokemon-card-modal']").should("exist").then(() => {
-            cy.get("[data-cy='pokemon-card-modal']").should("be.visible");
+        cy.get("[data-cy='pokemon-card-modal']").as("pokemonCard").should("exist").then(() => {
+            cy.get("@pokemonCard").should("be.visible");
         });
     });
 
     it("Should clear the input in the searchbox when simulating clicking on the native clear button in DOM", () => {
-        cy.get("[data-cy='navbar-search-input']").type("bLasToIsE");
-        cy.get("[data-cy='blastoise']").should("be.visible");
+        cy.get("[data-cy='navbar-search-input']").as("searchboxSearchInput").type("bLasToIsE").then(() => {
+            cy.get("[data-cy='blastoise']").should("be.visible");
 
-        cy.get("[data-cy='navbar-search-input']").clear();
-        cy.get("[data-cy='navbar-search-input']").should("have.value", "");
+            cy.get("@searchboxSearchInput").clear();
+            cy.get("@searchboxSearchInput").should("have.value", "");
+            cy.get("[data-cy='bulbasaur']").should("be.visible");
 
-        cy.get("[data-cy='bulbasaur']").should("be.visible");
-        cy.get("[data-cy='navbar-section']").click();
-        cy.get("[data-cy='bulbasaur']").should("not.exist");
+            cy.get("[data-cy='navbar-section']").click().then(() => {
+                cy.get("[data-cy='bulbasaur']").should("not.exist");
+                cy.get("[data-cy='navbar-dropdown-menu']").should("not.exist");
+            });
+        });
     });
 
     it("Should clear the input in the searchbox when simulating a backspace", () => {
-        cy.get("[data-cy='navbar-search-input']").type("asdsadsadsadadsad");
+        cy.get("[data-cy='navbar-search-input']").as("searchboxSearchInput").type("asdsadsadsadadsad");
 
-        cy.get("[data-cy='navbar-search-input']").type('{selectall}{backspace}').then(() => {
+        cy.get("@searchboxSearchInput").type('{selectall}{backspace}').then(() => {
             cy.get("[data-cy='navbar-section']").click();
             cy.get("[data-cy='navbar-dropdown-menu']").should("not.exist");
-            cy.get("[data-cy='navbar-search-input']").should("have.value", "");
+            cy.get("@searchboxSearchInput").should("have.value", "");
         });
     });
 
     it("Should display an empty list when a match is not found", () => {
         cy.get("[data-cy='navbar-search-input']").type("asdsadsadsadadsad");
 
-        cy.get("[data-cy='navbar-dropdown-menu']").should("be.visible").then(() => {
-            cy.get("[data-cy='navbar-dropdown-menu']").find("a").should("have.length", 0);
+        cy.get("[data-cy='navbar-dropdown-menu']").as("navbarDropdownMenu").should("be.visible").then(() => {
+            cy.get("@navbarDropdownMenu").find("a").should("have.length", 0);
         });
     });
 });
