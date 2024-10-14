2a4c49b0c4a7d0e16d01c0aba3ce51072525523e
Sebs5384 <operationmadjackal@hotmail.com>
2024-10-14 16:05:57 -0300

    Update grid cy spec with a new test case
    This will check for the grid only displaying the last two pokemons of the last page accordingly

--- cypress/e2e/grid.cy.js
+++ cypress/e2e/grid.cy.js
@@ -1,8 +1,9 @@
 describe("Grid interaction testing", () => {
     const localHost = "http://localhost:3000";
     const POKEMONS_PER_PAGE = 20;
-    const NEXT_PAGE_LIMIT = 20;
-    const LIMIT = 0;
+    const NEXT_PAGE_OFFSET = 20;
+    const LAST_PAGE_OFFSET = 1300;
+    const FIRST_PAGE_OFFSET = 0;
 
     const pageUrl = (limit, offset) => {
         return `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
@@ -15,7 +16,7 @@ describe("Grid interaction testing", () => {
         cy.visit(localHost);
         window.localStorage.clear();
 
-        cy.intercept("GET", pageUrl(POKEMONS_PER_PAGE, LIMIT), (req) => {
+        cy.intercept("GET", pageUrl(POKEMONS_PER_PAGE, FIRST_PAGE_OFFSET), (req) => {
             req.reply({
                 fixture: "pokedexFirstPage.json"
             });
@@ -36,11 +37,11 @@ describe("Grid interaction testing", () => {
                 });
 
                 cy.get("@gridBoard").find("strong").each(($strong, index) => {
-                    cy.wrap($strong).should("have.text", `#${index + 1} ${interception.response.body.results[index].name}`);
+                    cy.wrap($strong).should("have.text", `#${interception.response.body.results[index].url.split("/")[6]} ${interception.response.body.results[index].name}`);
                 });
 
                 cy.get("@gridBoard").find("img").each(($img, index) => {
-                    cy.wrap($img).should("have.attr", "src", pokemonSpriteUrl(index + 1));
+                    cy.wrap($img).should("have.attr", "src", pokemonSpriteUrl(interception.response.body.results[index].url.split("/")[6]));
                 });
             });
         });
@@ -49,7 +50,7 @@ describe("Grid interaction testing", () => {
     it("Should display the next 20 pokemons in the grid", () => {
         cy.wait(1000);
 
-        cy.intercept("GET", pageUrl(POKEMONS_PER_PAGE, NEXT_PAGE_LIMIT), (req) => {
+        cy.intercept("GET", pageUrl(POKEMONS_PER_PAGE, NEXT_PAGE_OFFSET), (req) => {
             req.reply({
                 fixture: "pokedexSecondPage.json"
             });
@@ -77,11 +78,47 @@ describe("Grid interaction testing", () => {
             });
 
             cy.get("@gridBoard").find("strong").each(($strong, index) => {
-                cy.wrap($strong).should("have.text", `#${index + 21} ${interception.response.body.results[index].name}`);
+                cy.wrap($strong).should("have.text", `#${interception.response.body.results[index].url.split("/")[6]} ${interception.response.body.results[index].name}`);
             });
 
             cy.get("@gridBoard").find("img").each(($img, index) => {
-                cy.wrap($img).should("have.attr", "src", pokemonSpriteUrl(index + 21));
+                cy.wrap($img).should("have.attr", "src", pokemonSpriteUrl(interception.response.body.results[index].url.split("/")[6]));
+            });
+        });
+    });
+
+    it("Should display the last two pokemons on the last page in the grid", () => {
+        cy.wait(1000);
+
+        cy.intercept("GET", pageUrl(POKEMONS_PER_PAGE, LAST_PAGE_OFFSET), (req) => {
+            req.reply({
+                fixture: "pokedexLastPage.json"
+            });
+        }).as("pokedexLastPage");
+
+        cy.get("[data-cy='grid-section']").as("gridSection").should("exist").then(() => {
+            cy.wait("@pokedexFirstPage").then((interception) => {
+                expect(interception.response.statusCode).to.eq(200);
+            });
+
+            cy.get("[data-cy='paginator-searchbox']").click().type("66{enter}");
+
+            cy.wait("@pokedexLastPage").then((interception) => {
+                expect(interception.response.statusCode).to.eq(200);
+
+                cy.get("[data-cy='grid-board']").as("gridBoard").should("exist").then(() => {
+                    cy.get("@gridBoard").find("div").should("have.length", interception.response.body.results.length);
+                    cy.get("@gridBoard").find("img").should("have.length", interception.response.body.results.length);
+                    cy.get("@gridBoard").find("strong").should("have.length", interception.response.body.results.length);
+                });
+
+                cy.get("@gridBoard").find("strong").each(($strong, index) => {
+                    cy.wrap($strong).should("have.text", `#${interception.response.body.results[index].url.split("/")[6]} ${interception.response.body.results[index].name}`);
+                });
+
+                cy.get("@gridBoard").find("img").each(($img, index) => {
+                    cy.wrap($img).should("have.attr", "src", pokemonSpriteUrl(interception.response.body.results[index].url.split("/")[6]));
+                });
             });
         });
     });
