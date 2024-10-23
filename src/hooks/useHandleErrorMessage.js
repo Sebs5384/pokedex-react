c211b97375a5c901fdcbb7073a5164d61a767b8e
Sebs5384 <operationmadjackal@hotmail.com>
2024-10-23 16:59:05 -0300

    Update error handling hook
    This will prevent the need of calling so many if statmenets by just mapping through an object and checking the source type of component

--- src/hooks/useHandleErrorMessage.js
+++ src/hooks/useHandleErrorMessage.js
@@ -3,38 +3,50 @@ import { errorMessageReducer, initialErrorMessageState } from "../reducers/index
 
 function useHandleErrorMessage(cardError, caughtPokemonError, searchboxError, paginatorError) {
     const [state, dispatch] = useReducer(errorMessageReducer, initialErrorMessageState);
+    const errorSourceMap = {
+        card: {
+            message: "There was an error while loading the Pokemon Card, please try again later.",
+            visibilityType: "SET_CARD_ERROR_MESSAGE_VISIBILITY"
+        },
+        caughtPokemon: {
+            message: "There was an error while loading the caught Pokemon, please try again later.",
+            visibilityType: "SET_CAUGHT_POKEMON_ERROR_MESSAGE_VISIBILITY"
+        },
+        searchbox: {
+            message: "There was an error while loading the searchbox, please try again later.",
+            visibilityType: "SET_SEARCHBOX_ERROR_MESSAGE_VISIBILITY"
+        },
+        paginator: {
+            message: "There was an error while loading the paginator, please try again later.",
+            visibilityType: "SET_PAGINATOR_ERROR_MESSAGE_VISIBILITY"
+        }
+    };
+    
 
     const handleCloseErrorMessage = () => {
-        dispatch({ type: "SET_CARD_ERROR_MESSAGE_VISIBILITY", payload: false})
-        dispatch({ type: "SET_CAUGHT_POKEMON_ERROR_MESSAGE_VISIBILITY", payload: false})
-        dispatch({ type: "SET_SEARCHBOX_ERROR_MESSAGE_VISIBILITY", payload: false})
-        dispatch({ type: "SET_PAGINATOR_ERROR_MESSAGE_VISIBILITY", payload: false})
+        Object.values(errorSourceMap).forEach(source => {
+            dispatch({ type: source.visibilityType, payload: false });
+        });
     };
 
     useEffect(() => {
-        if(cardError) {
-            dispatch({ type: "SET_ERROR_CAUSE_MESSAGE", payload: cardError.message });
-            dispatch({ type: "SET_ERROR_MESSAGE", payload: "There was an error while loading the Pokemon Card, please try again later." });
-            dispatch({ type: "SET_CARD_ERROR_MESSAGE_VISIBILITY", payload: true });
+        const errors = {
+            card: cardError,
+            caughtPokemon: caughtPokemonError,
+            searchbox: searchboxError,
+            paginator: paginatorError
         };
 
-        if(caughtPokemonError) {
-            dispatch({ type: "SET_ERROR_CAUSE_MESSAGE", payload: caughtPokemonError.message });
-            dispatch({ type: "SET_ERROR_MESSAGE", payload: "There was an error while loading the caught Pokemon, please try again later." });
-            dispatch({ type: "SET_CAUGHT_POKEMON_ERROR_MESSAGE_VISIBILITY", payload: true });
-        };
+        Object.keys(errors).forEach(key =>{
+            const error = errors[key];
+            const { message, visibilityType } = errorSourceMap[key];
 
-        if(searchboxError) {
-            dispatch({ type: "SET_ERROR_CAUSE_MESSAGE", payload: searchboxError.message });
-            dispatch({ type: "SET_ERROR_MESSAGE", payload: "There was an error while loading the searchbox, please try again later." });
-            dispatch({ type: "SET_SEARCHBOX_ERROR_MESSAGE_VISIBILITY", payload: true });
-        };
-
-        if(paginatorError) {
-            dispatch({ type: "SET_ERROR_CAUSE_MESSAGE", payload: paginatorError.message });
-            dispatch({ type: "SET_ERROR_MESSAGE", payload: "There was an error while loading the paginator, please try again later." });
-            dispatch({ type: "SET_PAGINATOR_ERROR_MESSAGE_VISIBILITY", payload: true });
-        };
+            if(error) {
+                dispatch({ type: "SET_ERROR_CAUSE_MESSAGE", payload: error.message });
+                dispatch({ type: "SET_ERROR_MESSAGE", payload: message });
+                dispatch({ type: visibilityType, payload: true });
+            };
+        });
     }, [cardError, caughtPokemonError, searchboxError, paginatorError]);
 
     return {
