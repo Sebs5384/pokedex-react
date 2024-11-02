ed3ca92b6871a36ae5a551e446e5a445620acc1b
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-11-02 01:39:11 -0600

    Add re-render case to Paginator
    This will test re-rendering of the Paginator when using the next button

--- src/components/Paginator/__test__/Paginator.spec.jsx
+++ src/components/Paginator/__test__/Paginator.spec.jsx
@@ -14,22 +14,67 @@ const Wrapper = ({ children, value }) => {
 describe("Paginator", () => {
     it("Should render paginator properly", async () => {
         const mockContextValue = {
-            currentPage: 1,
             totalPages: [1, 2, 3, 4, 5],
-            setItemRange: jest.fn(),
+            setItemRange: jest.fn()
+                .mockReturnValueOnce(false)
+                .mockReturnValueOnce(false)
+                .mockReturnValueOnce(false)
+                .mockReturnValueOnce(true)
+                .mockReturnValueOnce(true)
         };
         
         render(<Paginator />, { wrapper: ({ children }) => <Wrapper value={mockContextValue}>{children}</Wrapper> });
 
         const paginator = screen.getByTestId("pagination-section");
         expect(paginator).toBeInTheDocument();
+
         expect(screen.getByText("Previous")).toBeInTheDocument();
         expect(screen.getByText("Next")).toBeInTheDocument();
+        
         expect(screen.getByText("1")).toBeInTheDocument();
+        expect(screen.getByText("1")).not.toHaveClass("hidden")
         expect(screen.getByText("2")).toBeInTheDocument();
+        expect(screen.getByText("2")).not.toHaveClass("hidden")
         expect(screen.getByText("3")).toBeInTheDocument();
+        expect(screen.getByText("3")).not.toHaveClass("hidden")
+
         expect(screen.getByText("4")).toBeInTheDocument();
+        expect(screen.getByText("4")).toHaveClass("hidden")
         expect(screen.getByText("5")).toBeInTheDocument();
+        expect(screen.getByText("5")).toHaveClass("hidden")
+        expect(mockContextValue.setItemRange).toHaveBeenCalled();
+    });
+
+    it("Should change the visibility of pages when paginating", () => {
+        let currentPage = 1;
+        const mockContextValue = {
+            currentPage,
+            totalPages: [1, 2, 3, 4, 5],
+            setCurrentPage: jest.fn((page) => {
+                currentPage = page;
+            }),
+            setNextPage: jest.fn(),
+            setItemRange: jest.fn((item) => {
+                return !(currentPage >= item - 2 && currentPage <= item + 2);
+            })
+        };
+
+        const { rerender } = render(<Paginator />, { wrapper: ({ children }) => <Wrapper value={mockContextValue}>{children}</Wrapper> });
+
+        expect(screen.getByText("4")).not.toBeVisible();
+        expect(mockContextValue.setItemRange).toHaveBeenCalled();
+
+        const nextButton = screen.getByText("Next");
+        fireEvent.click(nextButton);
+        
+        mockContextValue.setCurrentPage(2);
+        expect(mockContextValue.setCurrentPage).toHaveBeenCalled();
+        expect(mockContextValue.setItemRange).toHaveBeenCalledWith(2, 1);
+        expect(currentPage).toEqual(2);
+    
+        rerender(<Paginator />, { wrapper: ({ children }) => <Wrapper value={mockContextValue}>{children}</Wrapper> });
+
+        expect(screen.getByText("4")).toBeVisible();
         expect(mockContextValue.setItemRange).toHaveBeenCalled();
     });
 });
