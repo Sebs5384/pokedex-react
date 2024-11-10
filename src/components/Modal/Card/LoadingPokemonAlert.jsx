7dd4d1abdc6890bf47d5951d679df8103e490377
Seb <linoshizu@gmail.com>
2024-11-10 16:51:46 -0600

    Update loading component
    We'll use test id to grab them in the test case when neccessary

--- src/components/Modal/Card/LoadingPokemonAlert.jsx
+++ src/components/Modal/Card/LoadingPokemonAlert.jsx
@@ -3,12 +3,21 @@ import PropTypes from "prop-types";
 
 function LoadingPokemonAlert({ showModal }) {
     return (
-        <PokemonAlertModal show={showModal} size="s" aria-labelledby="contained-modal-title-vcenter" data-cy={"loading-pokemon-alert"} centered>
+        <PokemonAlertModal 
+            show={showModal} 
+            size="s" 
+            aria-labelledby="contained-modal-title-vcenter" 
+            data-cy={"loading-pokemon-alert"} 
+            data-testid={"loading-pokemon-alert"}
+            centered
+        >
             <PokemonContainer className="modal-body">
                 <PokemonContainer className="alert-background d-flex justify-content-center align-items-center">
                     <PokemonContainer className="row w-100 justify-content-center">
                         <PokemonContainer className="col-12 text-center align-items-center alert-text-background">
-                            <PokemonCardText className="emerald-font">Loading....</PokemonCardText>
+                            <PokemonCardText className="emerald-font" data-testid="loading-text">
+                                Loading....
+                            </PokemonCardText>
                         </PokemonContainer>
                     </PokemonContainer>
                 </PokemonContainer>
