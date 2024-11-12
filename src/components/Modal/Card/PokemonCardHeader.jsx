998d341c1b93eb37af8cf84eb2cec4a75c220e56
Seb <linoshizu@gmail.com>
2024-11-12 15:30:04 -0300

    Add test id to pokemonCardHeader component
    We'll use this in the test suit to call upon the component elements

--- src/components/Modal/Card/PokemonCardHeader.jsx
+++ src/components/Modal/Card/PokemonCardHeader.jsx
@@ -6,16 +6,34 @@ function PokemonCardHeader({ selectedCard, pokemonSprite, closeModal, selectPrev
     return(
         <PokemonContainer closeButton data-testid={"pokemon-card-header"}>
             <PokemonSection className="container-fluid species-wrapper">
-                <PokemonContainer className="row species-data-wrapper" data-cy={"pokemon-card-header-genus-section"}>
-                    <PokemonCardText className="col-2 align-self-end text-end species-font">{pokemonSprite?.previous ? "P.STAGE" : ""}</PokemonCardText>
-                    <PokemonCardText className="col-5 align-self-end text-center species-font">{selectedCard?.evolutionGenus.name}</PokemonCardText>
-                    <PokemonCardText className="col-4 align-self-end text-end species-font">{selectedCard?.evolutionGenus.genus}</PokemonCardText>
-                    <PokemonCloseButton onClick={closeModal} data-cy={"pokemon-card-close-button"} data-testid={"pokemon-card-close-button"}/>
+                <PokemonContainer 
+                    className="row species-data-wrapper" 
+                    data-cy={"pokemon-card-header-genus-section"}
+                    data-testid={"pokemon-card-header-genus-section"}
+                >
+                    <PokemonCardText className="col-2 align-self-end text-end species-font">
+                        {pokemonSprite?.previous ? "P.STAGE" : ""}
+                    </PokemonCardText>
+                    <PokemonCardText className="col-5 align-self-end text-center species-font">
+                        {selectedCard?.evolutionGenus.name}
+                    </PokemonCardText>
+                    <PokemonCardText className="col-4 align-self-end text-end species-font">
+                        {selectedCard?.evolutionGenus.genus}
+                    </PokemonCardText>
+                    <PokemonCloseButton 
+                        onClick={closeModal} 
+                        data-cy={"pokemon-card-close-button"} 
+                        data-testid={"pokemon-card-close-button"}
+                    />
                 </PokemonContainer>
                 <PokemonContainer className="row justify-content-center">
                     <CardTopSeparator className="col-11" />
                 </PokemonContainer>
-                <PokemonTitle className="row" data-cy={"pokemon-card-header-main-section"}>
+                <PokemonTitle 
+                    className="row" 
+                    data-cy={"pokemon-card-header-main-section"}
+                    data-testid={"pokemon-card-header-main-section"}
+                >
                     { pokemonSprite?.previous ? 
                         <ImageContainer 
                             src={pokemonSprite?.previous} 
@@ -25,8 +43,12 @@ function PokemonCardHeader({ selectedCard, pokemonSprite, closeModal, selectPrev
                         />
                         : <PokemonContainer className="col-2 align-self-center text-center pokemon-species-image" />
                     }
-                    <PokemonCardText className="col-4 align-self-center text-center title-font capitalize" data-cy={"pokemon-card-header-name"}>{selectedCard?.name}</PokemonCardText>
-                    <PokemonCardText className="col-4 align-self-center text-end title-font hp-text">{selectedCard?.stats.hp}</PokemonCardText>
+                    <PokemonCardText className="col-4 align-self-center text-center title-font capitalize" data-cy={"pokemon-card-header-name"}>
+                        {selectedCard?.name}
+                    </PokemonCardText>
+                    <PokemonCardText className="col-4 align-self-center text-end title-font hp-text">
+                        {selectedCard?.stats.hp}
+                    </PokemonCardText>
                     <ImageContainer 
                         src={selectedCard?.typeImage.mainTypeIcon}
                         alt={selectedCard?.name} 
