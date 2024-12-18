4a9d91067c58bd9e03dcba2ffff517bfea473d1d
Seb <linoshizu@gmail.com>
2024-12-18 19:33:48 -0300

    Update handleCatchPokemon spec
    This will account for the textChange method inner the custome hook

--- src/hooks/__test__/useHandleCatchPokemon.spec.js
+++ src/hooks/__test__/useHandleCatchPokemon.spec.js
@@ -56,4 +56,76 @@ describe("useHandleCatchPokemon", () => {
             expect(result.current.isShaking).toBe(false);
         });
     });
+
+    it("Should handle text changes and modal visibilities when calling handleTextChange method", async () => {
+        const initialProps = {
+            pokemonsCount: 3,
+            pokemonList: ["testsaur", "testmander", "testrtle"],
+        };
+
+        useFetchPokemon.mockReturnValue({ caughtPokemonData: { name: "testmeleon" }, caughtPokemonError: null });
+        useFetchSpecies.mockReturnValue({ caughtPokemonSpeciesData: { name: "testmeleon" }, caughtPokemonSpeciesError: null });
+        useGetPokemonSprite.mockReturnValue({ caughtPokemonSprite: { current: "some-random.png", previous: null }});
+        getRandomPokemon.mockImplementation(() => "testmeleon");
+
+        const { result } = renderHook(
+            (props) => useHandleCatchPokemon(
+                props.pokemonsCount,
+                props.pokemonList
+            ), {
+                initialProps: initialProps,
+            }
+        );
+
+        expect(result.current.isShaking).toBe(false);
+        expect(result.current.caughtModalVisibility).toBe(false);
+        expect(result.current.textChange).toBe(true);
+
+        act(() => {
+            result.current.handlePokeballClick();
+        });
+
+        expect(result.current.isShaking).toBe(true);
+        expect(result.current.caughtModalVisibility).toBe(true);
+        expect(result.current.topText).toBe("Gotcha !");
+        expect(result.current.bottomText).toBe("TESTMELEON was caught");
+
+        act(() => {
+            jest.advanceTimersByTime(3000);
+        });
+
+        await waitFor(() => {
+            expect(result.current.textChange).toBe(false);
+            expect(result.current.topText).toBe("");
+            expect(result.current.bottomText).toBe("");
+        });
+
+        act(() => {
+            jest.advanceTimersByTime(500);
+        });
+
+        await waitFor(() => {
+            expect(result.current.textChange).toBe(true);
+            expect(result.current.topText).toBe("TESTMELEON'S data was");
+            expect(result.current.bottomText).toBe("added to the POKéDEX");
+        });
+
+        act(() => {
+            jest.advanceTimersByTime(3000);
+        });
+
+        await waitFor(() => {
+            expect(result.current.isShaking).toBe(false);
+            expect(result.current.caughtModalVisibility).toBe(false);
+            expect(result.current.registrationModalVisibility).toBe(true);
+        });
+
+        act(() => {
+            jest.advanceTimersByTime(10000);
+        });
+
+        await waitFor(() => {
+            expect(result.current.registrationModalVisibility).toBe(false);
+        });
+    });
 });
