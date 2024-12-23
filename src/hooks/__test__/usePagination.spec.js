22629c0be9c273247cfb1ebcd8246c35c9a538ad
Seb <linoshizu@gmail.com>
2024-12-22 23:01:58 -0300

    Separate test concerns
    We were previously testing side effects on while testing the setCurrentPage method, we are now testing them separately by mokcing just enough not to run side effects

--- src/hooks/__test__/usePagination.spec.js
+++ src/hooks/__test__/usePagination.spec.js
@@ -19,7 +19,7 @@ describe("usePagination", () => {
     const itemsPerPage = 20;
     const initialPageIndex = 1;
 
-    it("Should set the current page when invoking setCurrentPage", async () => {
+    it("Should set the pokemons in page correctly when running side effects", async () => {
         useFetchPokemons.mockReturnValue({
             paginatorPokemons: { 
                 results: [
@@ -44,33 +44,31 @@ describe("usePagination", () => {
             { name: "testbasaur", id: 2, sprite: "https://somerandom-sprite/2.png" },
             { name: "testrtle", id: 3, sprite: "https://somerandom-sprite/3.png" },
         ]);
+        const { result } = renderHook(() => usePagination(itemsPerPage, initialPageIndex));
+    
+        await waitFor(() => {
+            result.current.setPokemonsInPage();
+            expect(result.current.pokemonsInPage).toEqual([
+                { name: "testmander", id: 1, sprite: "https://somerandom-sprite/1.png" },
+                { name: "testbasaur", id: 2, sprite: "https://somerandom-sprite/2.png" },
+                { name: "testrtle", id: 3, sprite: "https://somerandom-sprite/3.png" },
+            ]);
+        });
+    });
 
+    it("Should set the current page when invoking setCurrentPage", async () => {
+        useFetchPokemons.mockReturnValue({
+            paginatorPokemons: {},
+        });
+        useTotalPages.mockReturnValue({
+            totalPages: 3,
+            firstPage: 1,
+            lastPage: 3,
+        });
         const { result } = renderHook(() => usePagination(itemsPerPage, initialPageIndex));
         
-        await waitFor(() => {
-            result.current.setPokemonsInPage();
-            expect(result.current.pokemonsInPage).toEqual([
-                { name: "testmander", id: 1, sprite: "https://somerandom-sprite/1.png" },
-                { name: "testbasaur", id: 2, sprite: "https://somerandom-sprite/2.png" },
-                { name: "testrtle", id: 3, sprite: "https://somerandom-sprite/3.png" },
-            ]);
-        });
-
-        act(() => {
-            result.current.setCurrentPage(2);
-        });
-
-        await waitFor(() => {
-            result.current.setPokemonsInPage();
-            expect(result.current.pokemonsInPage).toEqual([
-                { name: "testmander", id: 1, sprite: "https://somerandom-sprite/1.png" },
-                { name: "testbasaur", id: 2, sprite: "https://somerandom-sprite/2.png" },
-                { name: "testrtle", id: 3, sprite: "https://somerandom-sprite/3.png" },
-            ]);
-        });
+        act(() => result.current.setCurrentPage(2));
 
         expect(result.current.currentPage).toBe(2);
-        expect(result.current.totalPages).toBe(3);
-        expect(result.current.firstPage).toBe(1);
     });
 });
