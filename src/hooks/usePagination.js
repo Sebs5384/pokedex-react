4a38f9120850a117aa752722722367d1eaffd203
Seb <linoshizu@gmail.com>
2024-12-22 22:34:51 -0300

    Update usePagination logic
    It now takes the previously made getPokemonSprites and separates the logic of setPokemonsInPage function outside the side effect run

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,7 +1,7 @@
 import { useReducer, useEffect } from "react";
 import { paginationReducer, initialPaginationState } from "../reducers/index";
 import { useFetchPokemons, useTotalPages } from "./index";
-import { getPokemonsInPage, validateSearchboxPage, getSpriteUrl, loadSpriteUrl } from "../utils/index";
+import { getPokemonsInPage, validateSearchboxPage, getPokemonSprites } from "../utils/index";
 
 function usePagination(ITEMS_PER_PAGE, INITIAL_PAGE_INDEX) {
     const [state, dispatch] = useReducer(paginationReducer, initialPaginationState);
@@ -30,6 +30,19 @@ function usePagination(ITEMS_PER_PAGE, INITIAL_PAGE_INDEX) {
         dispatch({ type: "SET_SEARCHBOX_VALUE", payload: inputValue });
     };
 
+    const setPokemonsInPage = async () => {
+        if(paginatorPokemons && paginatorPokemons.results) {
+            const pokemonSprites = await getPokemonSprites(paginatorPokemons.results);
+            const pokemonsInPage = await getPokemonsInPage(paginatorPokemons, pokemonSprites);
+
+            dispatch({ type: "SET_POKEMONS_IN_PAGE", payload: pokemonsInPage });
+        } else {
+            setTimeout(() => {
+                dispatch({ type: "SET_NO_CARDS_IN_PAGE", payload: true });
+            }, 10000);
+        };
+    };
+
     const handleKeyDown = (event) => {
         if(event.key === "Enter") {
             const pageNumber = parseInt(state.searchboxValue);
@@ -51,24 +64,6 @@ function usePagination(ITEMS_PER_PAGE, INITIAL_PAGE_INDEX) {
     };
 
     useEffect(() => {
-        const setPokemonsInPage = async () => {
-            if(paginatorPokemons && paginatorPokemons.results) {
-                const sprites = await Promise.all(paginatorPokemons.results.map(async (pokemon) => { 
-                    const id = pokemon.url.split('/')[6];
-                    const sprite =  await loadSpriteUrl(getSpriteUrl(id))
-                
-                    return sprite
-                }));
-                const pokemonsInPage = await getPokemonsInPage(paginatorPokemons, sprites);
-                
-                dispatch({ type: "SET_POKEMONS_IN_PAGE", payload: pokemonsInPage });
-            } else {
-                setTimeout(() => {
-                    dispatch({ type: "SET_NO_CARDS_IN_PAGE", payload: true });
-                }, 10000);
-            };
-        };
-
         dispatch({ type: "SET_NEXT_PAGE_ITEMS", payload: nextOffset });
         setPokemonsInPage();
     }, [state.currentPage, ITEMS_PER_PAGE, nextOffset, paginatorPokemons]);
@@ -89,6 +84,7 @@ function usePagination(ITEMS_PER_PAGE, INITIAL_PAGE_INDEX) {
         setCurrentPage,
         setSearchboxValue,
         handleKeyDown,
+        setPokemonsInPage,
     };
 };
 
