74a8452badb4bb17a8311f5825f7b93764aa4f68
Seb <linoshizu@gmail.com>
2025-01-09 17:52:52 -0300

    Update useSelectedCard hook spec
    This will cover the case where the user selects a pokemon, previously we were expecting the results returned by parsePokemonData by mocking it, now we are mocking the class that we added in replace of said function

--- src/hooks/__test__/useSelectedCard.spec.js
+++ src/hooks/__test__/useSelectedCard.spec.js
@@ -3,12 +3,18 @@ import useSelectedCard from "../useSelectedCard";
 import useFetchPokemon from "../useFetchPokemon";
 import useFetchSpecies from "../useFetchSpecies";
 import useGetPokemonSprite from "../useGetPokemonSprite";
+import Pokemon from "../../entities/Pokemon";
 
 jest.mock("../useFetchPokemon", () => jest.fn());
 jest.mock("../useFetchSpecies", () => jest.fn());
 jest.mock("../useGetPokemonSprite", () => jest.fn());
+jest.mock("../../entities/Pokemon");
 
 describe("useSelectedCard", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it("Should reset the card data and then set it when invoking setSelectedCard", () => {
         useFetchPokemon.mockReturnValue({
             loading: false,
@@ -25,8 +31,52 @@ describe("useSelectedCard", () => {
         act(() => {
             result.current.setSelectedCard("testmeleon");
         });
+
         expect(result.current.cardData).toBe(null);
         expect(result.current.cardName).toBe("testmeleon");
+    });
+
+    it("Should set selected card data when fetched data is correct", () => {
+        const pokemonCardData = {
+            name: "testmeleon",
+            id: 2
+        };
+        const cardSpeciesData = {
+            evolves_from: "testmander",
+            id: 2
+        };
+        useFetchPokemon.mockReturnValue("")
+        .mockReturnValueOnce({
+            loading: false,
+        })
+        .mockReturnValueOnce({
+            loading: false,
+            pokemonCardData,
+        });
+        useFetchSpecies.mockReturnValue("")
+        .mockReturnValueOnce({
+            loadingSpecies: false,
+        })
+        .mockReturnValueOnce({
+            loadingSpecies: false,
+            cardSpeciesData
+        });
+        useGetPokemonSprite.mockReturnValue({
+            loadingSprite: false,
+            pokemonSprite: { 
+                current: "some-random.png", 
+                previous: null 
+            },
+        });
+        const { result } = renderHook(() => useSelectedCard("official-artwork"));
+
+        act(() => {
+            result.current.setSelectedCard("testmeleon");
+        });
+
+        expect(Pokemon).toHaveBeenCalledTimes(1);
+        expect(Pokemon).toHaveBeenCalledWith(pokemonCardData, cardSpeciesData);
+        expect(result.current.cardData).toBeInstanceOf(Pokemon);
     });
 
     it("Should set empty card data when fetched data is faulty", () => {
@@ -37,7 +87,6 @@ describe("useSelectedCard", () => {
         .mockReturnValueOnce({
             pokemonCardData: { name: "testmeleon" },
         });
-
         useFetchSpecies.mockReturnValue("")
         .mockReturnValueOnce({
             loadingSpecies: false,
@@ -45,11 +94,11 @@ describe("useSelectedCard", () => {
         .mockReturnValueOnce({
             cardSpeciesData: { evolves_from: "testmander" },
         });
-
         useGetPokemonSprite.mockReturnValue({
             pokemonSprite: { current: "some-random.png", previous: null },
         });
         const { result } = renderHook(() => useSelectedCard("official-artwork"));
+
         act(() => {
             result.current.setSelectedCard("testmeleon");
         });
